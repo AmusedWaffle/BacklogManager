0d2a9c8a227a3f769706a03c9f42ee8090cdcf8e
dzheng13341 <zhengd3@rpi.edu>
2025-03-21 10:58:36 -0400

    Cleaning up code

--- backlogmain.py
+++ backlogmain.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-import hashlib , requests, uuid
+import hashlib, requests, uuid
 #pip install flask flask_sqlalchemy psycopg2 flask_cors
 
 app = Flask(__name__)
@@ -33,12 +33,12 @@ class Users(db.Model):
     def __repr__(self):
         return f'<User {self.email}>'
     
+    #Hashes and stores the password.
     def set_password(self, password: str):
-        """Hashes and stores the password."""
         self.password_hash = hashlib.sha256(password.encode()).hexdigest()
 
+    #Checks if the given password matches the stored hash.
     def verify_password(self, input_password: str) -> bool:
-        """Checks if the given password matches the stored hash."""
         return self.password_hash == hashlib.sha256(input_password.encode()).hexdigest()
 
 #Temporary table
@@ -57,48 +57,44 @@ with app.app_context():
 CORS(app, resources={r"/*": {"origins": "http://128.113.126.87:3000"}})
 
 #API route to register a new user: http://127.0.0.1:5000/register
-
 @app.route('/register', methods=['POST'])
 def register():
     data = request.json
-    print(data)
+    #Checks if user exists first
     existing_user = Users.query.filter((Users.email == data['email'])).first()
     if existing_user:
         return jsonify({"error": "User already exists!"}), 400
-
+    #Adding new user
     new_user = Users(email=data['email'], name=data['name'])
     new_user.set_password(data['password'])
     token = generate_token()
     db.session.add(new_user)
     new_user.session_token = token
     db.session.commit()
-    
     return jsonify({'token': token}), 201
 
+#API route to login a user: http://127.0.0.1:5000/login
 @app.route('/login', methods=['POST'])
 def login():
     data = request.json
     user = Users.query.filter_by(email=data['email']).first()
-
+    #Checks if users and their password matches
     if user and user.verify_password(data['password']):
-        # Generate a new session token and store it in the database
         token = generate_token()
         user.session_token = token
         db.session.commit()
-
         return jsonify({'token': token}), 200
-
     return jsonify({'error': 'Invalid email or password'}), 401
 
-#Temp API route to add game for a user: http://127.0.0.1:5000/add_game
+#WPI API route to add game for a user: http://127.0.0.1:5000/add_game
 @app.route('/add_game', methods=['POST'])
 def add_game():
     data = request.json
     user_id = data['email']
     rawg_id = data['game']
 
+#Generate a new session token
 def generate_token():
-    """Generate a random session token."""
     return str(uuid.uuid4())
 
 if __name__ == '__main__':
