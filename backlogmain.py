7ada9fa1fe3b27bf1be175dff8cde998e8ce6512
dzheng13341 <zhengd3@rpi.edu>
2025-04-13 17:11:11 -0400

    Code cleanup and additional comments, seperated ranking algorithm for readability and organization

--- backlogmain.py
+++ backlogmain.py
@@ -1,26 +1,26 @@
-"""This module implements many different secure hash and message digest algorithms."""
+"""Main module for database management and routes to frontend."""
 import hashlib
 import uuid
-import requests
 import json
+import requests
 from flask import Flask, request, jsonify, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from sqlalchemy.sql.expression import func
-# pip install flask flask_sqlalchemy psycopg2 flask_cors
+from rankingservice import create_ranking
+# pip install flask flask_sqlalchemy psycopg2 flask_cors if first time running
+
 rankings = {}
 
 app = Flask(__name__)
 CORS(app)
 
 # Connection: MAKE SURE TO REPLACE USERNAME:PASSWORD WITH THE ONE SET UP ON YOUR OWN DEVICE
-# SSH Tunnel Forward to Local Port: ssh -L 5433:127.0.0.1:5432 USERNAME@128.113.126.87
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://knighk4:1234@localhost/backlog_manager'
 
 db = SQLAlchemy(app)
 
 RAWG_QUERY_URL = "https://api.rawg.io/api/games"
-RAWG_GAME_DETAILS_URL = "https://api.rawg.io/api/games/{}"
+RAWG_GAME_STAT = "https://api.rawg.io/api/games/{}"
 API_KEY = "1af69e1cf8664df59d23e49cd5aca2ea"
 
 '''
@@ -31,8 +31,15 @@ data = response.json()
 print(data)
 '''
 
+# Creates tables in the database (one-time setup for now)
+with app.app_context():
+    db.create_all()
+
+# Allow requests from frontend
+CORS(app, resources={r"/*": {"origins": "http://128.113.126.87:3000"}})
+
 class Users(db.Model):
-    """Define basic users table with columns: email, name, hashed passwords, and session token."""
+    """Users table with columns: email, name, hashed passwords, and session token."""
     email = db.Column(db.String(100), unique=True, nullable=False, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
@@ -49,8 +56,14 @@ class Users(db.Model):
         """Checks if the given password matches the stored hash."""
         return self.password_hash == hashlib.sha256(input_password.encode()).hexdigest()
 
+    def generate_token(self):
+        """Generates a new session token."""
+        self.session_token = str(uuid.uuid4())
+
 class Preferences(db.Model):
-    """Temporary table for user preferences."""
+    """Preferences table with solumns: email and preferences(json).
+    Example preferences: genres[], completionTime, esrbRating, platforms[], use_reviews 
+    """
     email = db.Column(db.String(100), unique=True, nullable=False, primary_key=True)
     preferences = db.Column(db.JSON, nullable=False)
 
@@ -64,6 +77,16 @@ class Library(db.Model):
 
     def __repr__(self):
         return f'<Library {self.email}>'
+
+    @classmethod
+    def find_a_users_game(cls, email, gameid):
+        """finds a users specific game based on given gameid"""
+        return cls.query.filter_by(email=email, gameid=gameid).first()
+
+    @classmethod
+    def find_a_users_gamelist(cls, email):
+        """finds all games owned by the user"""
+        return cls.query.filter_by(email=email).all()
 
 class Game(db.Model):
     """Game table with columns:
@@ -80,41 +103,32 @@ class Game(db.Model):
     def __repr__(self):
         return f'<Game {self.name}>'
 
-# Creates tables in the database (one-time setup for now)
-with app.app_context():
-    db.create_all()
-
-# Allow requests from frontend
-CORS(app, resources={r"/*": {"origins": "http://128.113.126.87:3000"}})
-
 @app.route('/register', methods=['POST'])
 def register():
     """API route to register a new user: http://127.0.0.1:5000/register."""
     data = request.json
-    # Checks if user exists first
+
     existing_user = Users.query.filter((Users.email == data['email'])).first()
     if existing_user:
         return jsonify({"error": "User already exists!"}), 400
-    # Adding new user
+
     new_user = Users(email=data['email'], name=data['name'])
     new_user.set_password(data['password'])
-    token = generate_token()
     db.session.add(new_user)
-    new_user.session_token = token
+    new_user.generate_token()
     db.session.commit()
-    return jsonify({'token': token}), 201
+    return jsonify({'token': new_user.session_token}), 201
 
 @app.route('/login', methods=['POST'])
 def login():
     """API route to login a user: http://127.0.0.1:5000/login."""
     data = request.json
     user = Users.query.filter_by(email=data['email']).first()
-    # Checks if users and their password matches
+
     if user and user.verify_password(data['password']):
-        token = generate_token()
-        user.session_token = token
+        user.generate_token()
         db.session.commit()
-        return jsonify({'token': token}), 200
+        return jsonify({'token': user.session_token}), 200
     return jsonify({'error': 'Invalid email or password'}), 401
 
 @app.route('/add-game', methods=['POST'])
@@ -128,8 +142,7 @@ def add_game():
     if not user:
         return jsonify({'error': 'Invalid token'}), 401
 
-    # Check if the user already owns the game
-    existing = Library.query.filter_by(email=user.email, gameid=rawg_id).first()
+    existing = Library.find_a_users_game(user.email, rawg_id)
     if existing:
         return jsonify({'message': 'Game already added'})
 
@@ -141,7 +154,7 @@ def add_game():
 
 @app.route('/delete-game', methods=['POST'])
 def delete_game():
-    """WPI API route to delete game for a user: http://127.0.0.1:5000/delete-game."""
+    """API route to delete game for a user: http://127.0.0.1:5000/delete-game."""
     data = request.json
     token = data['token']
     rawg_id = data['game_id']
@@ -150,8 +163,7 @@ def delete_game():
     if not user:
         return jsonify({'error': 'Invalid token'}), 401
 
-    # Check if the user already owns the game
-    game = Library.query.filter_by(email=user.email, gameid=rawg_id).first()
+    game = Library.find_a_users_game(user.email, rawg_id)
     if not game:
         return jsonify({'message': 'Game not found in library'})
 
@@ -162,22 +174,22 @@ def delete_game():
 
 @app.route('/get-games-library', methods=['POST', 'OPTIONS'])
 def get_game_library():
-    """WPI API route to add game for a user: http://127.0.0.1:5000//get-games-library."""
+    """API route to add game for a user: http://127.0.0.1:5000/get-games-library."""
     if request.method == 'OPTIONS':
         return '', 200
     data = request.json
     token = data['token']
 
-    # Verify the token
     user = Users.query.filter_by(session_token=token).first()
     if not user:
         return jsonify({'error': 'Invalid token'}), 401
 
-    user_games = Library.query.filter_by(email=user.email).all()
+    user_games = Library.find_a_users_gamelist(user.email)
     game_ids = [entry.gameid for entry in user_games]
     results = []
+
     for game_id in game_ids:
-        response = requests.get(RAWG_GAME_DETAILS_URL.format(game_id), params={'key': API_KEY})
+        response = requests.get(RAWG_GAME_STAT.format(game_id), params={'key': API_KEY}, timeout=15)
         if response.status_code == 200:
             game_data = response.json()
             results.append({'id': game_data['id'], 'name': game_data['name']})
@@ -186,21 +198,23 @@ def get_game_library():
 
 @app.route('/search-games', methods=['POST'])
 def search_games():
-    """WPI API route to add game for a user: http://127.0.0.1:5000//search-games."""
+    """API route to add game for a user: http://127.0.0.1:5000/search-games."""
     data = request.json
     search_query = data['query']
     params = {
         'key': API_KEY,  
         'search': search_query
     }
-    response = requests.get(RAWG_QUERY_URL, params=params)
+    response = requests.get(RAWG_QUERY_URL, params=params, timeout=15)
     results = response.json().get('results', [])
     games = [{'id': game['id'], 'name': game['name']} for game in results]
     return jsonify({'results': games})
 
 @app.route('/save-default-preferences', methods=['POST'])
 def save_default_preferences():
-    """Saves user preferences to the database."""
+    """API route to save user preferences to the database:
+    http://127.0.0.1:5000/save-default-preferences.
+    """
     data = request.json
     token = data['token']
     preferences = data['preferences']
@@ -221,7 +235,9 @@ def save_default_preferences():
 
 @app.route('/parse-preferences', methods=['POST'])
 def parse_preferences():
-    """Ranks games based on the user's owned games and preferences, then sends ranking to frontend."""
+    """API route to ranks games based on the user's owned games and preferences:
+    http://127.0.0.1:5000/parse-preferences.
+    """
     if request.method == 'OPTIONS':
         return '', 200
     data = request.json
@@ -232,18 +248,19 @@ def parse_preferences():
     if not user:
         return jsonify({'error': 'Invalid token'}), 401
 
-    user_games = Library.query.filter_by(email=user.email).all()
+    user_games = Library.find_a_users_gamelist(user.email)
     if not user_games:
         print("nope")
         return jsonify({'error': 'No owned games found'}), 404
 
     game_ids = [game.gameid for game in user_games]
-    create_ranking(preferences, game_ids, user)
+    ranked_games = create_ranking(preferences, game_ids)
+    rankings[user.email] = ranked_games
     return jsonify({"message": "Ranking sent to frontend"})
 
 @app.route('/receive-ranking', methods=['POST'])
 def receive_ranking():
-    """Returns the stored ranking of games."""
+    """API route to send users ranking of games: http://127.0.0.1:5000/receive-ranking."""
     if request.method == 'OPTIONS':
         return '', 200
     data = request.json
@@ -259,9 +276,10 @@ def receive_ranking():
 
 @app.route('/get-game-stats', methods=['POST'])
 def get_game_stats():
+    """API route to gather a game's detail: http://127.0.0.1:5000/get-game-stats."""
     data = request.json
     game_id = data['game_id']
-    response = requests.get(RAWG_GAME_DETAILS_URL.format(game_id), params={'key': API_KEY})
+    response = requests.get(RAWG_GAME_STAT.format(game_id), params={'key': API_KEY}, timeout=15)
     if response.status_code == 200:
         game_data = response.json()
         game_stats = {'title': game_data['name'],
@@ -271,45 +289,8 @@ def get_game_stats():
                       'developer': [dev['name'] for dev in game_data['developers']],
                       'genres': [genre['name'] for genre in game_data['genres']],
                       'platforms': [plat['platform']['name'] for plat in game_data['platforms']],
-                      'reviews': game_data['ratings']}     
+                      'reviews': game_data['ratings']}
     return jsonify({'game_stats': game_stats})
-
-def create_ranking(preferences, game_ids, user):
-    """Creates a game ranking."""
-    ranked_games = []
-    for game_id in game_ids:
-        response = requests.get(RAWG_GAME_DETAILS_URL.format(game_id), params={'key': API_KEY})
-        if response.status_code == 200:
-            game_data = response.json()
-            ranked_games.append({'id': game_id, 'weight': weight_game(preferences, game_data), 'name': game_data['name']})
-    ranked_games.sort(key=lambda x: x['weight'], reverse=True)
-    rankings[user.email] = ranked_games
-    return ranked_games
-
-def weight_game(preferences, game_data):
-    """Weights a game based on user preferences."""
-    weight = 100
-    if type(preferences['completionTime']) == int and type(game_data['playtime']) == int:
-        weight = weight - abs(preferences['completionTime'] - game_data['playtime'])
-    if game_data['esrb_rating'] != None and preferences['esrbRating'] != None:
-        if game_data['esrb_rating']['name'] in preferences['esrbRating']:
-            weight += 50
-    if preferences['platforms'] != None:
-        for platform in game_data['platforms']:
-            if platform['platform']['name'] in preferences['platforms']:
-                weight += 50
-    if preferences['genre'] != None:
-        for genre in game_data['genres']:
-            if genre['name'] in preferences['genre']:
-                weight += 50
-    if game_data['metacritic'] != None:
-        if preferences['use_reviews']:
-            weight += game_data['metacritic']
-    return weight
-
-def generate_token():
-    """Generates a new session token."""
-    return str(uuid.uuid4())
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=False)
